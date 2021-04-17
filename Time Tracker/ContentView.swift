2c7078a341341a19a3f399b8ef5a27d141b2f15e
Mohammad Naanaa <hamudi.naanaa@tum.de>
2021-04-17 19:41:24 +0200

    Initial Commit

---
+++ Time Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time Tracker
+//
+//  Created by Hamudi Naanaa on 17.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time Tracker/Time_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_TrackerApp.swift
+//  Time Tracker
+//
+//  Created by Hamudi Naanaa on 17.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Time_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
